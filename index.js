19ba8a9fbbbae081f328320ca02186d50cc4790a
Prince-gk <prince.georanja@gmail.com>
2024-11-11 12:34:50 +0300

    json

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 6001; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+console.log("Listening on port 6001");
